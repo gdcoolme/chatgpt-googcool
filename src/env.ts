e8e952fdda69d011c73cb677940e2e3f1a499848
gdcoolme <128764301+gdcoolme@users.noreply.github.com>
2023-05-06 13:46:34 +0800

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -21,8 +21,6 @@ export const defaultEnv = {
   CLIENT_DEFAULT_MESSAGE: `Powered by OpenAI Vercel
 - 如果本项目对你有所帮助，可以给小猫 [买点零食](http://www.gdcool.net/gdpay.jpg)，但不接受任何付费功能请求。
 - 本项目由 [@果冻](http://www.gdcool.net) 基于 [chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发。本网站仅作为项目演示，不提供服务，API Key由我自己免费提供，珍惜调用次数，注意不要滥用。
-- 点击每条消息前的头像，可以锁定对话，作为角色设定。
-- 现在支持多个对话，打开对话设置，点击新建对话。在输入框里输入 [[/]][[/]] 或者 [[空格]][[空格]] 可以切换对话，搜索历史消息。
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。
 `,
   CLIENT_MAX_INPUT_TOKENS: {
